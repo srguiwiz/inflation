5012d71686fdcd47521e88c89ab49f9d17894b73
Leo Baschy <srguiwiz12@nrvnr.com>
2021-04-26 18:40:39 -0700

    Localize for one more country

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ or can be installed from the browser to your phone's home screen.
 | ![US icon](us/inflation.svg) | US | https://inflation.cmprh.com/us/ | US$ |
 | ![CA icon](ca/inflation-ca.svg) | Canada | https://inflation.cmprh.com/ca/ | CA$ |
 | ![DE icon](de/inflation-de.svg) | Germany | https://inflation.cmprh.com/de/ | € after DM |
+| ![FR icon](fr/inflation-fr.svg) | France | https://inflation.cmprh.com/fr/ | € after F |
 | ![CH icon](ch/inflation-ch.svg) | Switzerland | https://inflation.cmprh.com/ch/ | Fr. |
 | ![AT icon](at/inflation-at.svg) | Austria | https://inflation.cmprh.com/at/ | € after ÖS |
 
@@ -40,6 +41,7 @@ The US, Canadian, and Swiss inflation calculators have been configured for their
 single currency, respectively.
 A historical currency switch is handled correctly in the German and Austrian
 calculators, as an example.
+Two switches are handled in the French calculator, as an example.
 The code for multiple currencies is present equally in all calculators.
 
 This calculator is optimized for certain uses.
---
+++ fr/sw.js
@@ -0,0 +1,35 @@
+// forks should have a distinct cacheName, for example including the country code for which it is,
+// see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+const cacheName = "inflation-fr-annual";
+
+self.addEventListener("install", (installEvent) => {
+  async function createAndFillCache() {
+    const cache = await caches.open(cacheName);
+    await cache.addAll([
+      "./",
+      "./sw.js",
+      "./manifest.json",
+      "./inflation-fr.svg",
+      "./inflation-fr-48x48.png",
+      "./inflation-fr-72x72.png",
+      "./inflation-fr-96x96.png",
+      "./inflation-fr-128x128.png",
+      "./inflation-fr-256x256.png",
+      "./inflation-fr-512x512.png",
+    ]);
+  }
+  installEvent.waitUntil(createAndFillCache());
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  async function fetchOnlineOrFromCache() {
+    if (fetchEvent.request.method !== "GET") return;
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(fetchEvent.request);
+    if (cachedResponse) {
+      return cachedResponse.clone();
+    }
+    return fetch(fetchEvent.request);
+  }
+  fetchEvent.respondWith(fetchOnlineOrFromCache());
+});
