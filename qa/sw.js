d245249f9280e2ba7645e15ba5090cbe46902f02
Leo Baschy <srguiwiz12@nrvnr.com>
2021-04-28 05:37:31 -0700

    Support another writing system

---
+++ qa/sw.js
@@ -0,0 +1,35 @@
+// forks should have a distinct cacheName, for example including the country code for which it is,
+// see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+const cacheName = "inflation-qa-annual";
+
+self.addEventListener("install", (installEvent) => {
+  async function createAndFillCache() {
+    const cache = await caches.open(cacheName);
+    await cache.addAll([
+      "./",
+      "./sw.js",
+      "./manifest.json",
+      "./inflation-qa.svg",
+      "./inflation-qa-48x48.png",
+      "./inflation-qa-72x72.png",
+      "./inflation-qa-96x96.png",
+      "./inflation-qa-128x128.png",
+      "./inflation-qa-256x256.png",
+      "./inflation-qa-512x512.png",
+    ]);
+  }
+  installEvent.waitUntil(createAndFillCache());
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  async function fetchOnlineOrFromCache() {
+    if (fetchEvent.request.method !== "GET") return;
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(fetchEvent.request);
+    if (cachedResponse) {
+      return cachedResponse.clone();
+    }
+    return fetch(fetchEvent.request);
+  }
+  fetchEvent.respondWith(fetchOnlineOrFromCache());
+});
