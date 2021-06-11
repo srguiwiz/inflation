92c2801e4e58ba57c5bd922b7e4b35afdb285dbe
Leo Baschy <srguiwiz12@nrvnr.com>
2021-06-11 16:15:35 -0700

    Refine localizations

--- de/sw.js
+++ de/sw.js
@@ -1,4 +1,4 @@
-// v 2.5.6
+// v 2.6.0
 
 // forks should have a distinct cacheName, for example including the country code for which it is,
 // see https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
